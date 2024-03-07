a33e027c6a271bc3ecea825e7feb9a930666fafa
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-03-07 13:39:15 -0300

    fixing deploy problems

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,28 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Switch } from "@headlessui/react";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export default function Example() {
-  const [agreed, setAgreed] = useState(false);
-
+export default function Contact() {
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
