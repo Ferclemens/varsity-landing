feb7f5126d1266895d7fe8a08fe89a14310b93d3
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-04-29 18:47:42 -0300

    navbar mobile theme bug fixed

--- src/components/LangButton.tsx
+++ src/components/LangButton.tsx
@@ -19,7 +19,7 @@ function LangSwitch() {
     <div>
       <button
         className="text-sm font-bold leading-6 text-gray-900 dark:text-gray-200 hover:text-blue-800 
-        transition-colors text-shadow border border-blue-300 rounded-sm px-3"
+        transition-colors  border border-blue-300 rounded-sm px-3"
         onClick={() => changeLanguage()}
       >
         {btnName}
