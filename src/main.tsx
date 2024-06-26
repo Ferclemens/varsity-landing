21910652eb9328d9105d38a8890bf858203535ad
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-06-26 10:20:09 -0300

    back and modified .htaccess file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
