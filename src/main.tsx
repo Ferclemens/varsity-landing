42bc4ee9151b7dc7d1e578e7979b076b5e29fd30
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-06-26 09:13:53 -0300

    React.StrictMode tag deleted in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n.js";
 
-let page = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(page);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
